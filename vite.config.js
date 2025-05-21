e59116fb7a16f6ade485e7027f30fe64fd3119a6
Thyago <thyagorps@gmail.com>
2025-05-21 00:26:48 -0300

    deploy 2

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,4 @@
-import { defineConfig } from 'vite'
-
-export default defineConfig({
-  // GitHub Pages expects an index.html in the root directory
-  // so just run npm build before pushing to GitHub and this will rebuild our assets to the root
-  build: { outDir: '..' },
-  // needed for github pages just put the repo name here
-  base: '/serenatto/', 
-});
+export default {
+  // config options
+  base: '/serenatto/',
+}
